673a1150f8ede8392e45eb43752678e026d42659
Lukas Seiwald <lseiwald.mmt-m2019@fh-salzburg.ac.at>
2021-04-25 15:54:04 +0200

    feat: 🎸 added opinion form which updates a firebase database

---
+++ src/components/Form/Form.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import Firebase from 'firebase';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      developers: []
+    };
+    this.onChangeGender = this.onChangeGender.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.writeUserData();
+    }
+  }
+
+  writeUserData = () => {
+    Firebase.database()
+      .ref('/')
+      .set(this.state);
+    console.log('DATA SAVED');
+  };
+
+  getUserData = () => {
+    let ref = Firebase.database().ref('/');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let opinion = this.refs.opinion.value;
+    let gender = this.refs.gender.value;
+    console.log(this.refs.gender.value)
+    let age = this.refs.age.value;
+    let uid = this.refs.uid.value;
+
+    if (uid && opinion && age && gender) {
+      const { developers } = this.state;
+      const devIndex = developers.findIndex(data => {
+        return data.uid === uid;
+      });
+      developers[devIndex].opinion = opinion;
+      developers[devIndex].gender = gender;
+      developers[devIndex].age = age;
+      this.setState({ developers });
+    } else if (opinion && age && gender) {
+      const uid = new Date().getTime().toString();
+      const { developers } = this.state;
+      developers.push({ uid, opinion, gender, age });
+      this.setState({ developers });
+    }
+
+    this.refs.opinion.value = '';
+    this.refs.gender.value = '';
+    this.refs.age.value = '';
+    this.refs.uid.value = '';
+  };
+
+  removeData = developer => {
+    const { developers } = this.state;
+    const newState = developers.filter(data => {
+      return data.uid !== developer.uid;
+    });
+    this.setState({ developers: newState });
+  };
+
+  updateData = developer => {
+    this.refs.uid.value = developer.uid;
+    this.refs.opinion.value = developer.opinion;
+    this.refs.gender.value = developer.gender;
+    this.refs.age.value = developer.age;
+  };
+
+  // onChangeGender = developer => {
+  //   console.log("test")
+  //   console.log(developer.gender)
+  //   this.refs.gender.value = developer.gender;
+  // }
+
+  onChangeGender(event) {
+    this.refs.gender.value = event.target.value
+    // console.log(event.target.value);
+  }
+
+  render() {
+    const { developers } = this.state;
+    return (
+      <React.Fragment>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xl-12'>
+              <h1></h1>
+              <form onSubmit={this.handleSubmit}>
+                <div className='form-row'>
+                  <input type='hidden' ref='uid' />
+                  <div className='form-group col-md-6'>
+                    <label>aufgetischt:</label>
+                    <textarea
+                      type='text'
+                      ref='opinion'
+                      maxLength='30'
+                      className='form-control'
+                      placeholder='Red nicht lange um den heißen Brei. Bring deine Meinung in 30 Zeichen auf den Punkt.'
+                    />
+                  </div>
+                  <div className='form-group col-md-6' ref='gender' onChange={this.onChangeGender}>
+                    <input type="radio" name="gender" value="m" /> M
+                    <input type="radio" name="gender" value="f" /> F
+                    <input type="radio" name="gender" value="o" /> O
+                  </div>
+                  <div className='form-group col-md-6'>
+                    <label>Age:</label>
+                    <input
+                      type='number'
+                      ref='age'
+                      className='form-control'
+                      placeholder='Age'
+                    />
+                  </div>
+                </div>
+                <button type='submit' className='btn btn-primary'>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-xl-12'>
+              <h1>
+                Teller:
+              </h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-xl-12'>
+              {developers.map(developer => (
+                <div
+                  key={developer.uid}
+                  className='card float-left'
+                  style={{ width: '18rem', marginRight: '1rem' }}
+                >
+                  <div className='card-body'>
+                    <h5 className='card-title'>{developer.opinion}</h5>
+                    <p className='card-text'>{developer.gender}</p>
+                    <p className='card-text'>{developer.age}</p>
+                    <button
+                      onClick={() => this.removeData(developer)}
+                      className='btn btn-link'
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBOoRF94KTVva5_tvyHh5wN--fYvgBOEuw',
+  authDomain: 'aufgetischt-a8cc6.firebaseapp.com',
+  projectId: 'aufgetischt-a8cc6',
+  databaseURL: 'https://aufgetischt-a8cc6-default-rtdb.europe-west1.firebasedatabase.app',
+  storageBucket: 'aufgetischt-a8cc6.appspot.com',
+  messagingSenderId: '978121975652',
+  appId: '1:978121975652:web:66a46c1a2adede6d91fc71'
+});
 
 ReactDOM.render(
   <React.StrictMode>
