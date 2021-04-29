ac2c3ca8c7990863992685fdf59f6a3323359620
Lukas Seiwald <lseiwald.mmt-m2019@fh-salzburg.ac.at>
2021-04-29 16:22:14 +0200

    feat: 🎸 added categories to form and changed styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Firebase from 'firebase';
 import './Form.css';
+import Plate from '../Plate/Plate'
 
 class Form extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class Form extends React.Component {
       opinions: []
     };
     this.onChangeGender = this.onChangeGender.bind(this);
+    this.onChangeCategory = this.onChangeCategory.bind(this);
   }
 
   componentDidMount() {
@@ -40,28 +42,32 @@ class Form extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
     let text = this.refs.text.value;
+    let category = this.refs.category.value || '-' ;
     let gender = this.refs.gender.value || '-' ;
-    console.log(this.refs.gender.value)
     let age = this.refs.age.value || '-' ;
     let uid = this.refs.uid.value;
 
-    if (uid && text && age && gender) {
+    if (uid && text && age && gender && category) {
+      console.log('zyessss')
+
       const { opinions } = this.state;
       const index = opinions.findIndex(data => {
         return data.uid === uid;
       });
       opinions[index].text = text;
+      opinions[index].category = category;
       opinions[index].gender = gender;
       opinions[index].age = age;
       this.setState({ opinions });
-    } else if (text && age && gender) {
+    } else if (text && age && gender && category) {
       const uid = new Date().getTime().toString();
       const { opinions } = this.state;
-      opinions.push({ uid, text, gender, age });
+      opinions.push({ uid, text, category, gender, age });
       this.setState({ opinions });
     }
 
     this.refs.text.value = '';
+    this.refs.category.value = '';
     this.refs.gender.value = '';
     this.refs.age.value = '';
     this.refs.uid.value = '';
@@ -78,13 +84,17 @@ class Form extends React.Component {
   updateData = opinion => {
     this.refs.uid.value = opinion.uid;
     this.refs.text.value = opinion.text;
+    this.refs.category.value = opinion.category;
     this.refs.gender.value = opinion.gender;
     this.refs.age.value = opinion.age;
   };
 
   onChangeGender(event) {
     this.refs.gender.value = event.target.value
-    // console.log(event.target.value);
+  }
+
+  onChangeCategory(event) {
+    this.refs.category.value = event.target.value
   }
 
   render() {
@@ -94,23 +104,48 @@ class Form extends React.Component {
         <div className='container'>
           <div className='row'>
             <div className='col-xl-12'>
-              <h1></h1>
               <form onSubmit={this.handleSubmit}>
+                <div className='form-row'>
+                  <p className='title-category'>Wähle dein Thema:</p>
+                  <div className='col-md-6 form-categories' ref='category' onChange={this.onChangeCategory}>
+                    <input
+                      type='radio'
+                      name='category'
+                      value='covid'
+                      id='radio-covid'
+                      className='radioButton'
+                    />
+                    <label for='radio-covid'>COVID-19</label>
+                    <input
+                      type='radio'
+                      name='category'
+                      value='equality'
+                      id='radio-equality'
+                      className='radioButton'
+                    />
+                    <label for='radio-equality'>GLEICHBERECHTIGUNG</label>
+                    <input
+                      type='radio'
+                      name='category'
+                      value='other'
+                      id='radio-other'
+                      className='radioButton'
+                    />
+                    <label for='radio-other'>ANDERES</label>
+                  </div>
+                </div>
                 <p id='form-title'>aufgetischt:</p>
                 <div className='form-row'>
                   <input type='hidden' ref='uid' />
-                  {/* <div className='form-group col-md-6'>
-                    <label>WÄHLE DEIN THEMA:</label>
-                    
-                  </div> */}
                   <div className='form-group col-md-6 text-area'>
                     <textarea
                       type='text'
                       ref='text'
-                      maxLength='45'
+                      maxLength='35'
                       className='form-control'
                       required
-                      placeholder='Red nicht lange um den heißen Brei. \n Bring deine Meinung in 30 Zeichen auf den Punkt.'
+                      placeholder='Red nicht lange um den heißen Brei.                                                    
+                      Bring deine Meinung in 30 Zeichen auf den Punkt.'
                     />
                   </div>
                 </div>
@@ -177,15 +212,19 @@ class Form extends React.Component {
                   style={{ width: '18rem', marginRight: '1rem' }}
                 >
                   <div className='card-body'>
-                    <h5 className='card-title'>{opinion.text}</h5>
-                    <p className='card-text'>{opinion.gender}</p>
-                    <p className='card-text'>{opinion.age}</p>
-                    <button
-                      onClick={() => this.removeData(opinion)}
-                      className='btn btn-link'
-                    >
-                      Delete
-                    </button>
+                    <Plate text={opinion.text} />
+                    <div className='detail'>
+                      <h5 className='card-title'>{opinion.text}</h5>
+                      <p className='card-text'>Thema: {opinion.category}</p>
+                      <p className='card-text'>Gender: {opinion.gender}</p>
+                      <p className='card-text'>Alter: {opinion.age}</p>
+                      <button
+                        onClick={() => this.removeData(opinion)}
+                        className='btn btn-link'
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 </div>
               ))}
--- src/components/Plate/Plate.js
+++ src/components/Plate/Plate.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import './Plate.css';
 
-class Form extends React.Component {
+class Plate extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
     const { text } = this.props;
+    let colors = ['#4D4D4D', '#999999', '#FFFFFF', '#F44E3B', '#FE9200', '#FCDC00', '#DBDF00', '#A4DD00', '#68CCCA', '#73D8FF', '#AEA1FF', '#FDA1FF', '#333333', '#808080', '#cccccc', '#D33115', '#E27300', '#FCC400', '#B0BC00', '#68BC00', '#16A5A5', '#009CE0', '#7B64FF', '#FA28FF', '#000000', '#666666', '#B3B3B3', '#9F0500', '#C45100', '#FB9E00', '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E'];
+    let color = colors[Math.floor(Math.random() * colors.length)];
+
     return (
-      <svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
-        <g>
-          <title>Layer 1</title>
-          <ellipse stroke-width="5" stroke="#000" ry="280.99999" rx="280.99999" id="svg_1" cy="300.00001" cx="399.99999" fill="#c146c1"/>
-          <ellipse stroke-width="5" stroke="#000" ry="204" rx="204" id="svg_2" cy="300" cx="399" fill="#fff"/>
-          <text font-style="normal" font-weight="bold" xml:space="preserve" text-anchor="start" font-family="monospace" font-size="30" id="svg_11" y="307" x="246" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#000000">{text}</text>
-        </g>
+      <svg width={800} height={600} xmlns="http://www.w3.org/2000/svg" className="plate">
+        <circle
+          strokeWidth={5}
+          stroke="#000"
+          cy={300}
+          cx={400}
+          fill={color}
+          r={281}
+        />
+        <circle
+          strokeWidth={5}
+          stroke="#000"
+          cy={300}
+          cx={399}
+          fill="#fff"
+          r={204}
+        />
+        <text
+          fontWeight="bold"
+          fontFamily="monospace"
+          fontSize={30}
+          y={307}
+          x={246}
+          fillOpacity="null"
+          strokeOpacity="null"
+          strokeWidth={0}
+          stroke="#000"
+        >
+          {text}
+        </text>
       </svg>
     )
   }
 }
 
-
-
+export default Plate;
