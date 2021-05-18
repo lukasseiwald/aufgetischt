8392633672ac2b473c0e3431543a51e5e85418db
Lukas Seiwald <lseiwald.mmt-m2019@fh-salzburg.ac.at>
2021-05-18 06:27:41 +0200

    feat: 🎸 new about content added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,9 @@ import './About.css';
 import {
   Link,
 } from 'react-router-dom';
-import AufgetischtLogo from '../../assets/aufgetischt_logo.js'
-
+import AufgetischtLogo from '../../assets/aufgetischt_logo.js';
+import foodImg from '../../assets/about/food.jpg';
+import SaltPepperSvg from '../../assets/about/salz_pfeffer';
 class About extends React.Component {
 
   render() {
@@ -20,15 +21,83 @@ class About extends React.Component {
           </Link>
           <div className='about-content'>
             <AufgetischtLogo />
-            <p id='about-description'>
-              aufgetischt ist eine Kampagne für mehr Diskurs und Meinungsvielfalt.
+            <div className='text-image-container'>
+              <p className='about-description'>
+                Ist ein visuelles Werkzeug zur Darstellung aktueller Meinungsbilder am Tisch. 
+              </p>
+              <img src={foodImg} className='aboutImg' />
+            </div>
+            <div className='text-image-container'>
+              <img src={foodImg} className='aboutImg' />
+              <p className='about-description description-2'>
+                Anonym und niederschwellig werden verschiedene Meinungen abseits des Tischgesprächs abgebildet und eine Reaktion darauf abgefragt. 
+              </p>
+            </div>
+            <div className='text-image-container'>
+              <div className='about-sidenote-container'>
+                <AufgetischtLogo />
+                <p className='about-description-small'>
+                  war zu Beginn als ein Upcyclingprojekt angedacht und hat sich zu einer Kampagne mit einem Werkzeug für die visualisierung vielfältiger Meinungen am Tisch entwickelt.
+                </p>
+              </div>
+              <p className='about-description description-2'>
+                Es soll gezeigt werden das verschiedene Positionen am Tisch sitzen. Ziel ist es den inneren Monolog eine neue Richtung zu geben.
+              </p>
+            </div>
+            <div className='text-image-container'>
+              <div className='about-filler' />
+              <p className='about-description description-2'>
+                Wir wollen beobachten welche Grauzonen dabei entstehen. Gibt es mehr als Schwarz und Weiss?
+              </p>
+            </div>
+            <div className='text-image-container'>
+              <div className='about-filler' />
+              <div className='form-categories about-categories'>
+                <input
+                  type='radio'
+                  name='category-gallery'
+                  className='radioButton category-radio'
+                />
+                <label>UMWELTSCHUTZ</label>
+                <input
+                  type='radio'
+                  name='category-gallery'
+                  className='radioButton category-radio'
+                />
+                <label>FEMINISMUS</label>
+                <input
+                  type='radio'
+                  name='category-gallery'
+                  className='radioButton category-radio'
+                />
+                <label>RASSISMUS</label>
+                <input
+                  type='radio'
+                  name='category-gallery'
+                  className='radioButton category-radio'
+                />
+                <label>MENTALE GESUNDHEIT</label>
+              </div>
+            </div>
+            <p className='full-length-description'>
+              Die zur Auswahl stehenden Themen sind entweder noch nicht vom Tisch oder es gibt dafür nicht ausreichend Platz. Die Fronten sind verhärtet. 
             </p>
-            <p id='about-description'>
-              Wir tischen verschiedene Meinungen auf und servieren damit Vielfalt. Denn diese wird durch den fehlenden Austausch beschnitten. Wir wirken der Gruppenpolarisation, Blasenbildung und den Echokammern entgegen um mehr Grauzonen zu schaffen.
+            <p className='full-length-description-2'>
+              Also tisch uns Deine Meinung auf und vergiss nicht: Dein Tellerrand ist nicht das Ende, es gibt den ganzen Tisch!
             </p>
-            <p id='about-description'>
-              Dein Tellerrand ist nicht das Ende, es gibt den ganzen Tisch!
-            </p>
+            <Link
+              className='button-submit about-button'
+              to={{pathname: '/'}}>
+              <p className='aboutButtonTextMobile'>
+                Ich will auftischen
+              </p>
+            </Link>
+            <div className='beilage-container about-beilage'>
+              <SaltPepperSvg />
+              <p>
+                Wir möchten kein Salz in alte Wunden streuen, wissen allerdings das wir uns auf einen schmalen Grad befinden. Deshalb möchten wir uns entschuldigen, sollte Dir etwas nicht bekommen. Wir denken aber, dass es wichtig ist im Gespräch zu bleiben und sich mit Meinungen zu befassen welche sich nicht mit unseren eigenen Ansichten decken.
+              </p>
+            </div>
           </div>
         </div>
       </React.Fragment>
